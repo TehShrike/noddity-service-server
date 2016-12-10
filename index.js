a8f95138b376208b91941de42f5b5f21736f5516
Josh Duff <me@JoshDuff.com>
2016-12-10 16:03:02 -0600

    Bump level/butler versions, move levelup dbs to tmp

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-var url = require('url')
-var qs = require('querystring')
-var StringMap = require('stringmap')
-var Butler = require('noddity-butler')
-var sanitize = require("sanitize-filename")
-var Linkifier = require('noddity-linkifier')
-var level = require('level')
+const url = require('url')
+const qs = require('querystring')
+const StringMap = require('stringmap')
+const Butler = require('noddity-butler')
+const sanitize = require("sanitize-filename")
+const Linkifier = require('noddity-linkifier')
+const level = require('level')
+const tmpdir = require('os').tmpdir()
+const joinPath = require('path').join
 
 function dumbResolve(firstThingy, secondThingy) {
-	var separator = '/'
-	if (firstThingy[firstThingy.length - 1] === '/') {
-		separator = ''
-	}
+	const startsWithSlash = firstThingy[firstThingy.length - 1] === '/'
+	const separator = startsWithSlash ? '' : '/'
+
 	return firstThingy + separator + secondThingy
 }
 
@@ -20,26 +21,25 @@ function bareMinimumParametersExist(parameters) {
 }
 
 module.exports = function(serverImplementation) {
-	var butlers = new StringMap()
+	const butlers = new StringMap()
 
 	function getAppropriateButler(rootUrl) {
 		if (!butlers.has(rootUrl)) {
-			var prefix = process.env.BRANCH || 'noddity-service-server-2'
-			var db = level(prefix + sanitize(rootUrl))
-			var butler = new Butler(rootUrl, db)
+			const db = level(joinPath(tmpdir, sanitize(rootUrl)))
+			const butler = new Butler(rootUrl, db)
 			butlers.set(rootUrl, butler)
 		}
 
 		return butlers.get(rootUrl)
 	}
 
-	var server = require('http').createServer(function(req, res) {
-		var parameters = qs.parse(url.parse(req.url).query)
+	const server = require('http').createServer(function(req, res) {
+		const parameters = qs.parse(url.parse(req.url).query)
 		if (bareMinimumParametersExist(parameters)) {
-			var butler = getAppropriateButler(parameters.noddityRoot)
-			var linkify = new Linkifier(parameters.postUrlRoot)
+			const butler = getAppropriateButler(parameters.noddityRoot)
+			const linkify = new Linkifier(parameters.postUrlRoot)
 
-			var thingsTheServerImplementationCouldCareAbout = {
+			const thingsTheServerImplementationCouldCareAbout = {
 				url: req.url,
 				parameters: parameters,
 				butler: butler,
@@ -47,7 +47,7 @@ module.exports = function(serverImplementation) {
 				resolvePost: dumbResolve.bind(null, parameters.postUrlRoot)
 			}
 
-			serverImplementation(thingsTheServerImplementationCouldCareAbout, function (err, html) {
+			serverImplementation(thingsTheServerImplementationCouldCareAbout, function(err, html) {
 				if (err) {
 					res.statusCode = 500
 					res.end(err.message)
@@ -60,11 +60,11 @@ module.exports = function(serverImplementation) {
 		}
 	})
 
-	var closeServer = server.close.bind(server)
+	const closeServer = server.close.bind(server)
 
 	server.close = function close() {
 		Object.keys(butlers.obj).forEach(function(key) {
-			var butler = butlers.get(key)
+			const butler = butlers.get(key)
 			butler.stop()
 		})
 		closeServer()
